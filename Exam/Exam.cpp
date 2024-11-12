779449e6e1b6bdd3632e9972839767a739c2a66b
4b3g0132@office.stust.edu.tw <4b3g0132@office.stust.edu.tw>
2024-11-12 11:24:32 +0800

    以for 結構計算小於等於n的所有偶數和

--- Exam/Exam.cpp
+++ Exam/Exam.cpp
@@ -4,8 +4,15 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n = 1;
+    int sum = 0;
+    std::cout << "請輸入一個正整數n :";
+    std::cin >> n;
 
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) sum += i;
+    }
+    std::cout << "1~" << n << "之間所有偶數的和為: " << sum << std::endl;
     return 0;
 }
 
