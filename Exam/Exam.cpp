870d843824c13076fa7e491bca2bdd2be106f979
4b3g0132@office.stust.edu.tw <4b3g0132@office.stust.edu.tw>
2024-11-12 10:52:22 +0800

    判斷月份

--- Exam/Exam.cpp
+++ Exam/Exam.cpp
@@ -4,7 +4,29 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int month = 1;
+
+    std::cout << "請輸入月份(1~12月) : ";
+    std::cin >> month;
+    if (month > 12 || month < 1) {
+        std::cout << "請輸入正確的月份" << std::endl;
+        std::cout << "請輸入月份(1~12月) : ";
+        std::cin >> month;
+    }
+    else {
+        if (month >= 3 && month <= 5) {
+            std::cout << "春季" << std::endl;
+        }
+        else if (month >= 6 && month <= 8) {
+            std::cout << "夏季" << std::endl;
+        }
+        else if (month >= 9 && month <= 11) {
+            std::cout << "秋季" << std::endl;
+        }
+        else {
+            std::cout << "冬季" << std::endl;
+        }
+    }
 
     return 0;
 }
