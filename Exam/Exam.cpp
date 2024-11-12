18fa529274f3143069bf23bbac6028f8080e1599
4b3g0132@office.stust.edu.tw <4b3g0132@office.stust.edu.tw>
2024-11-12 14:50:23 +0800

    利用 For loop、While loop、Do-While loop 計算小於等於n的所有偶數的和

--- Exam/Exam.cpp
+++ Exam/Exam.cpp
@@ -9,16 +9,30 @@ int main()
     std::cout << "請輸入一個正整數n :";
     std::cin >> n;
 
-    //for loop
-    for (int i = 1; i <= n; i++) {
-        if (i % 2 == 0) sum += i;
+    //For loop
+    for (int i = 2; i <= n; i+=2) {
+        sum += i;
     }
+    std::cout << "For Loop: " << "1~" << n << "之間所有偶數的和為: " << sum << std::endl;
 
-    //while loop
+    //While loop
+    int i = 1;
+    sum = 0;
+    while (i <= n) {
+        if (i % 2 == 0) sum += i;
+        i++;
+    }
+    std::cout << "While Loop: " << "1~" << n << "之間所有偶數的和為: " << sum << std::endl;
 
+    //Do-While loop
+    i = 2;
+    sum = 0;
+    do {
+        sum += i;
+        i+=2;
+    } while (i <= n);
+    std::cout << "Do-While loop: " << "1~" << n << "之間所有偶數的和為: " << sum << std::endl;
 
-
-    std::cout << "1~" << n << "之間所有偶數的和為: " << sum << std::endl;
     return 0;
 }
 
